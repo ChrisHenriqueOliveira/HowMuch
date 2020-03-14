bc94b31bc7245da4c80d69be39861a2685a9cba4
ChrisHenriqueOliveira <christian1101999@gmail.com>
2020-03-14 02:56:30 -0300

    Adding things

--- constants/theme.js
+++ constants/theme.js
@@ -1,7 +1,7 @@
 const colors = {
     accent: "#F3534A",
-    primary: "#0AC4BA",
-    secondary: "#2BDA8E",
+    primary: "#E93354",
+    secondary: "#1D55C5",
     tertiary: "#FFE358",
     black: "#323643",
     white: "#FFFFFF",
--- navigation/index.js
+++ navigation/index.js
@@ -10,7 +10,7 @@ import { theme } from '../constants'
 const screens = createStackNavigator({
     Welcome,
 }, {
-    defaultNavigationOprions: {
+    defaultNavigationOptions: {
         headerStyle: {},
         headerBackImage: <Image />,
         headerBackTitle: null,
